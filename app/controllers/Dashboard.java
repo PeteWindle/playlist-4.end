c739000e10600da5554725eead3d0f218dc3ccda
pwindle <pwindle@wit.ie>
2023-04-25 23:06:37 +0100

    add bulma version of playlist 4

---
+++ app/controllers/Dashboard.java
@@ -0,0 +1,35 @@
+package controllers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import models.Playlist;
+import models.Song;
+import play.Logger;
+import play.mvc.Controller;
+
+public class Dashboard extends Controller
+{
+  public static void index()
+  {
+    Logger.info("Rendering Admin");
+
+    List<Playlist> playlists = Playlist.findAll();
+    render ("dashboard.html", playlists);
+  }
+  public static void deletePlaylist (Long id)
+  {
+    Playlist playlist = Playlist.findById(id);
+    Logger.info ("Removing" + playlist.title);
+    playlist.delete();
+    redirect ("/dashboard");
+  }
+  public static void addPlaylist (String title)
+  {
+    Playlist playlist = new Playlist (title, 0);
+    Logger.info ("Adding a new playlist called " + title);
+    playlist.save();
+    redirect ("/dashboard");
+  }
+}
+
